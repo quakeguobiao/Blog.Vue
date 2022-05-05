3eee19d9458b4fbbbae28fbe5639a153b5f030e9
quakeguobiao <quakeguobiao@gmail.com>
2022-05-05 17:12:43 +0800

    根据实际项目修改授权ids路径与配置

--- src/Auth/applicationusermanager.js
+++ src/Auth/applicationusermanager.js
@@ -3,12 +3,15 @@ import { UserManager } from 'oidc-client'
 class ApplicationUserManager extends UserManager {
   constructor () {
     super({
-      authority: 'https://ids.neters.club',
+      //authority: 'https://ids.neters.club',
+      authority: 'http://ids.banlantech.com',
       client_id: 'blogvuejs',
-      redirect_uri: 'http://vueblog.neters.club/callback',
+        /*redirect_uri: 'http://vueblog.neters.club/callback',*/
+        redirect_uri: 'http://testbusiness.banlantech.com/callback',
       response_type: 'id_token token',
       scope: 'openid profile roles blog.core.api.BlogModule',
-      post_logout_redirect_uri: 'http://vueblog.neters.club'
+        //post_logout_redirect_uri: 'http://vueblog.neters.club'
+        post_logout_redirect_uri: 'http://testbusiness.banlantech.com'
     })
   }
 
